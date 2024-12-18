9657f579d55d47b16c0b59dcee5b30946b286d5e
z-Eduard005 <eduardpysanka@gmail.com>
2024-12-19 01:59:24 +0200

    added firebase instead of data.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import fs from "fs/promises";
 // import dotenv from "dotenv";
 import express from "express";
 import TGBot from "node-telegram-bot-api";
+import { initializeApp } from "firebase/app";
+import { get, getDatabase, ref, set } from "firebase/database";
 
 // dotenv.config({ path: ".env.local" });
 const port = process.env.PORT || 3000;
@@ -9,33 +10,48 @@ const tgbToken = process.env.TELEGRAM_BOT_TOKEN || "";
 const chatId = Number(process.env.CHAT_ID) || 0;
 const apiPassword = process.env.API_PASSWORD || "";
 const allowedIPs = JSON.parse(process.env.ALLOWED_IPS || "[]");
-const DATA_FILE_PATH = "./data.json";
 const QUERY_TIMEOUT_DURATION_SEC = 50;
 let queryTimeout = null;
 const app = express();
 app.use(express.json());
+
+// firebase
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY || "",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
+  databaseURL: process.env.FIREBASE_DB_URL || "",
+  projectId: process.env.FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.FIREBASE_SENDER_ID || "",
+  appId: process.env.FIREBASE_APP_ID || "",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+const getServerStatus = async () => {
+  const snapshot = await get(ref(database, "status-server-data/"));
+  return snapshot.val();
+};
+
+const updateServerStatus = async (status) => {
+  await set(ref(database, "status-server-data/"), status);
+};
 
 const checkPassword = (req, res, next) => {
   if (req.headers["x-api-password"] === apiPassword) return next();
   return res.status(403).json({ error: "Access denied" });
 };
 
-async function getServerStatus() {
-  const data = await fs.readFile(DATA_FILE_PATH, "utf8");
-  return JSON.parse(data);
-}
-
-async function updateServerStatus(status) {
-  await fs.writeFile(DATA_FILE_PATH, JSON.stringify(status, null, 2));
-}
-
 const resetTimeout = () => {
   if (queryTimeout) clearTimeout(queryTimeout);
 
   queryTimeout = setTimeout(async () => {
     try {
-      await updateServerStatus({ on: false, ipv4: null });
-      bot.sendMessage(chatId, "<b>Server closed!</b>", { parse_mode: "HTML" });
+      await updateServerStatus({ on: false, ipv4: "" });
+      bot.sendMessage(chatId, "<i><b>Server closed!</b></i>", {
+        parse_mode: "HTML",
+      });
     } catch (err) {
       console.error("Error updating server status:", err);
     }
@@ -79,7 +95,7 @@ app.post("/set", checkPassword, async (req, res) => {
       if (!(await getServerStatus().on))
         bot.sendMessage(
           chatId,
-          `Server open! On IP: <b>${newStatus.ipv4}:25565</b>`,
+          `Join in! Server open on IP: <i><b>${newStatus.ipv4}:25565</b></i>`,
           { parse_mode: "HTML" }
         );
     } catch (err) {
