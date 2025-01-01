23bf488b59aee5cd904739bc42aec59a87a165b0
Eduard <147715855+z-Eduard005@users.noreply.github.com>
2025-01-02 00:46:54 +0200

    fix: content-type in /check route

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const checkPassword = (req, res, next) => {
 app.post("/check", checkPassword, async (req, res) => {
   try {
     const ipv4DB = await getServerStatusDB();
-    const ipv4 = req.body;
+    const { ipv4 } = req.body;
     const ok = allowedIPs.some((pc) => pc.ip === ipv4);
     if (!ok) res.status(403).json({ err: "You are not allowed to play!" });
     res.json(ipv4DB);
